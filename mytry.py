e4710bc8ec454f1424aad94a1c5f5d4ee94ebb43
Nehemiah A Mensah <mensahnehemiah@gmail.com>
2023-10-03 14:05:19 +0000

    initialcommit

---
+++ mytry.py
@@ -0,0 +1,35 @@
+# def add_two(num):
+#     result = num + 2
+#     return
+
+# user_input = ("Enter your number ")
+# answer = add_two(user_input)
+# print(f"Your answer is {answer}")
+
+info = input("Enter your age ")
+# # print(info)
+# # print(type(info))
+# # print(info.isnumeric())
+
+# while info.isnumeric() != True and info == float():
+#     print("ERROR!!! \nPlease enter a valid input")
+#     info = input("Enter your age ")
+# else: level = input("What is your name? ")
+# while level.isalpha() != True:
+#     print("ERROR!!! \n Enter a name without a number")
+#     level = input("What is your name? ")
+# else: print(f"Your age is {info} \n Your name is {level}")
+if "." in info:
+    print("True")
+    while info.isnumeric() != True :
+        print("ERROR!!! \nPlease enter a valid input")
+        info = input("Enter your age ")
+    else: level = input("What is your name? ")
+    while level.isalpha() != True:
+        print("ERROR!!! \n Enter a name without a number")
+        level = input("What is your name? ")
+    else: print(f"Your age is {info} \n Your name is {level}")
+
+
+
+
