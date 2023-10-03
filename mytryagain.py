1f94915a31f7d78cef848c368640ba2b9b07ca84
Nehemiah A Mensah <mensahnehemiah@gmail.com>
2023-10-03 14:05:55 +0000

    initial commit

---
+++ mytryagain.py
@@ -0,0 +1,20 @@
+#SOLID S--- single responsibility  eg. using one method / class to solve one problem
+# O - open for extension close to modification - dont modify the code a lot
+# listov 
+
+# def validation(val):
+#     if val.isnumeric() == True:
+#         return val + 10
+#     else: print("Not valid")
+
+# entry = input("Enter the number: ")
+# entry2 = validation(entry)
+
+# print(entry2)
+
+guf = input("Enter number ")
+# guf = guf.isnumeric()
+# print(type(guf))
+
+# if guf.isnumeric() == True:
+#     print("You did it")
